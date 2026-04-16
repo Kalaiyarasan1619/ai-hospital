2011e5a0151208f0225f24550d176040c6014701
Kalaiyarasan1619 <155789550+Kalaiyarasan1619@users.noreply.github.com>
2026-04-16 23:57:26 +0530

    importing

--- hospital-backendprocess/eureka/src/main/java/com/example/controller/HealthController.java
+++ hospital-backendprocess/eureka/src/main/java/com/example/controller/HealthController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @CrossOrigin(origins = {
